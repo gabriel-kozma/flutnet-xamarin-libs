1911f61aeb23b1fe283c3081e04ec9bc0b414b09
Mark Fang <microspaze@qq.com>
2024-06-07 15:45:08 +0800

    Refine flutter engine init way.

--- samples/Flutnet.Sample.iOS/AppDelegate.cs
+++ samples/Flutnet.Sample.iOS/AppDelegate.cs
@@ -7,9 +7,12 @@ namespace Flutnet.Sample
     [Register("AppDelegate")]
     public class AppDelegate : FlutterAppDelegate
     {
+        private FlutterEngine _flutterEngine = new FlutterEngine("flutnet");
+
         [Export("application:didFinishLaunchingWithOptions:")]
         public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
         {
+            _flutterEngine.Run();
             return base.FinishedLaunching(application, launchOptions);
         }
     }
